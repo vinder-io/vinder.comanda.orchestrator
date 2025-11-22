582f6f78d74523eea8e6b7619ddf1a459ae2549d
Richard Garcia <code.richardy@gmail.com>
2025-11-22 17:08:58 -0300

    this commit introduces comments providing greater clarity and links to further details.

--- Source/Vinder.Comanda.Orchestrator.Infrastructure/Policies/PollyPolicies.cs
+++ Source/Vinder.Comanda.Orchestrator.Infrastructure/Policies/PollyPolicies.cs
@@ -4,6 +4,10 @@ public static class PollyPolicies
 {
     public static AsyncPolicyWrap<Result<TResult>> CreatePolicy<TResult>(ILogger logger)
     {
+        // rate Limiting restricts the number of requests to an external service,
+        // protecting it from overload and ensuring fair usage.
+
+        // more details: https://learn.microsoft.com/en-us/azure/architecture/patterns/rate-limiting-pattern
         var rateLimitPolicy = Policy.RateLimitAsync(
             numberOfExecutions: 5,
             perTimeSpan: TimeSpan.FromSeconds(1),
@@ -26,6 +30,10 @@ public static class PollyPolicies
             .Handle<Exception>()
             .WaitAndRetryAsync(2, _ => TimeSpan.FromSeconds(2));
 
+        // circuit Breaker prevents calling an external service after repeated failures,
+        // protecting the system and giving time to recover.
+
+        // more details: https://learn.microsoft.com/en-us/azure/architecture/patterns/circuit-breaker
         var circuitBreakerPolicy = Policy.Handle<Exception>()
             .CircuitBreakerAsync(exceptionsAllowedBeforeBreaking: 3, durationOfBreak: TimeSpan.FromSeconds(20),
                 onBreak: (exception, breakDelay) =>
