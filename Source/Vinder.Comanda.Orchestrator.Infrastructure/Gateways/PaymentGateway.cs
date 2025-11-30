19db2fba31116b17bf2d7143b3a3953ddd5f3ff2
Richard Garcia <code.richardy@gmail.com>
2025-11-30 13:38:53 -0300

    feature: this commit introduces the implementation of the communication gateway interface with the “comanda” payment service

---
+++ Source/Vinder.Comanda.Orchestrator.Infrastructure/Gateways/PaymentGateway.cs
@@ -0,0 +1,49 @@
+﻿namespace Vinder.Comanda.Orchestrator.Infrastructure.Gateways;
+
+public sealed class PaymentGateway(IPaymentClient paymentClient, ILogger<IPaymentGateway> logger) : IPaymentGateway
+{
+    private readonly AsyncPolicyWrap<Result<PaymentScheme>> _localPaymentPolicy =
+        PollyPolicies.CreatePolicy<PaymentScheme>(logger);
+
+    private readonly AsyncPolicyWrap<Result<CheckoutSession>> _onlinePaymentPolicy =
+        PollyPolicies.CreatePolicy<CheckoutSession>(logger);
+
+    private readonly AsyncPolicyWrap<Result<PaginationScheme<PaymentScheme>>> _paymentsFetchPolicy =
+        PollyPolicies.CreatePolicy<PaginationScheme<PaymentScheme>>(logger);
+
+    public async Task<Result<PaymentScheme>> CreateLocalPaymentAsync(
+        OfflinePaymentScheme parameters, CancellationToken cancellation = default)
+    {
+        // applies a full resiliency pattern for external service calls using
+        // timeout, retry, fallback, and circuit breaker policies.
+
+        // more details: https://learn.microsoft.com/dotnet/architecture/resilient-applications/
+        return await _localPaymentPolicy.ExecuteAsync(token =>
+            paymentClient.CreateLocalPaymentAsync(parameters, token), cancellation
+        );
+    }
+
+    public async Task<Result<CheckoutSession>> CreateOnlineChargeAsync(
+        CheckoutSessionCreationScheme parameters, Credential credential, CancellationToken cancellation = default)
+    {
+        // applies a full resiliency pattern for external service calls using
+        // timeout, retry, fallback, and circuit breaker policies.
+
+        // more details: https://learn.microsoft.com/dotnet/architecture/resilient-applications/
+        return await _onlinePaymentPolicy.ExecuteAsync(token =>
+            paymentClient.CreateOnlineChargeAsync(parameters, credential, token), cancellation
+        );
+    }
+
+    public async Task<Result<PaginationScheme<PaymentScheme>>> GetPaymentsAsync(
+        PaymentsFetchParameters parameters, CancellationToken cancellation = default)
+    {
+        // applies a full resiliency pattern for external service calls using
+        // timeout, retry, fallback, and circuit breaker policies.
+
+        // more details: https://learn.microsoft.com/dotnet/architecture/resilient-applications/
+        return await _paymentsFetchPolicy.ExecuteAsync(token =>
+            paymentClient.GetPaymentsAsync(parameters, token), cancellation
+        );
+    }
+}
