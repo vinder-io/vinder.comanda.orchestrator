1c95496fc0251dd623a61336eeecafec26816bd3
Richard Garcia <code.richardy@gmail.com>
2025-11-30 13:38:23 -0300

    feature: this commit introduces a new interface to represent the communication gateway with the comanda payment service.

---
+++ Source/Vinder.Comanda.Orchestrator.Application/Gateways/IPaymentGateway.cs
@@ -0,0 +1,20 @@
+﻿namespace Vinder.Comanda.Orchestrator.Application.Gateways;
+
+public interface IPaymentGateway
+{
+    public Task<Result<PaginationScheme<PaymentScheme>>> GetPaymentsAsync(
+        PaymentsFetchParameters parameters,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result<CheckoutSession>> CreateOnlineChargeAsync(
+        CheckoutSessionCreationScheme parameters,
+        Credential credential,
+        CancellationToken cancellation = default
+    );
+
+    public Task<Result<PaymentScheme>> CreateLocalPaymentAsync(
+        OfflinePaymentScheme parameters,
+        CancellationToken cancellation = default
+    );
+}
