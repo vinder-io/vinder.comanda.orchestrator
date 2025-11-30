e4c790978c127901d56b5ec5fc82e86268c2eaba
Richard Garcia <code.richardy@gmail.com>
2025-11-30 13:39:19 -0300

    feature: introduces global usage directives for the payment payloads namespace

--- Source/Vinder.Comanda.Orchestrator.Infrastructure/Usings.cs
+++ Source/Vinder.Comanda.Orchestrator.Infrastructure/Usings.cs
@@ -4,6 +4,7 @@ global using Vinder.Comanda.Internal.Contracts.Errors;
 global using Vinder.Comanda.Internal.Contracts.Clients.Interfaces;
 
 global using Vinder.Comanda.Internal.Contracts.Transport.Internal.Profiles;
+global using Vinder.Comanda.Internal.Contracts.Transport.Internal.Payments;
 global using Vinder.Comanda.Internal.Contracts.Transport.Internal;
 
 global using Vinder.Comanda.Orchestrator.Application.Gateways;
